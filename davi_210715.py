2e09db1a5ba6071cb961205162c35cddbce04647
davicova <86981409+davicova@users.noreply.github.com>
2021-07-15 15:07:52 -0300

    Add files via upload

---
+++ davi_210715.py
@@ -0,0 +1,39 @@
+print("2 *******************************")
+Frutas=["abacaxi","uva","melancia","kiwi","laranja"]
+print("3 *******************************")
+print(Frutas)
+print("4 *******************************")
+print(Frutas[1])
+print("5 *******************************")
+Frutas.sort()
+print("6 *******************************")
+print(Frutas)
+print("7 *******************************")
+Frutas.append("apple")
+print("8 *******************************")
+print(Frutas)
+print("9 *******************************")
+Frutas.insert(0,"abobrinha")
+print("10 *******************************")
+print(Frutas)
+print("11 *******************************")
+fruta2=sorted(Frutas)
+print("12 *******************************")
+print(Frutas)
+Frutas.append("limao")
+print("13 *******************************")
+print(fruta2)
+print(Frutas)
+print("14 *******************************")
+for item in Frutas:
+    print(item.title())
+Frutas.append("banana")
+print(Frutas)
+for item in Frutas:
+    print(item.title())
+
+    
+
+
+
+
